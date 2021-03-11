19ad3d1f4f166b0cb5c7b24a8dbf272df6063d43
Barbara Isabel Herrera Flores <bherrerf@everis.com>
2021-03-11 12:31:50 +0100

    conection check, models and context check, everything ok

---
+++ Models/Genero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ApiLibros.Models
+{
+    public partial class Genero
+    {
+        public Genero()
+        {
+            Libros = new HashSet<Libro>();
+        }
+
+        public int Id { get; set; }
+        public string Genero1 { get; set; }
+
+        public virtual ICollection<Libro> Libros { get; set; }
+    }
+}
