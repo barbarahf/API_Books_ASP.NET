3a931ef915385880cd732aa43067d0c1ca24b30c
Barbara Isabel Herrera Flores <bherrerf@everis.com>
2021-03-16 13:00:40 +0100

    preview

--- front_end_react/src/index.js
+++ front_end_react/src/index.js
@@ -3,28 +3,25 @@ import ReactDOM from 'react-dom'; //To render
 import './css/index.css';
 
 //SPA route
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+//import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals';
 import Header from './Header';
 import Client from './cliente';
 //import { Link } from 'react-router'; //Para añadir rutas
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 ReactDOM.render(
-   /* <Router history={BrowserRouter}>
-        <Route path="/" component={Header}/>
-        <Route path="/cliente" component={Cliente}/>
-    </Router>*/
-    /*    <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Header}/>
-                <Route path="/cliente" component={Client}/>
-            </Switch>
-        </BrowserRouter>,*/
-     <React.StrictMode>
-            <Header/>
-        </React.StrictMode>,
-
+    <Router>
+        <div>
+            <Route exact path="/">
+                <Header/>
+            </Route>
+            <Route exact path="/cliente">
+                <Client/>
+            </Route>
+        </div>
+    </Router>,
     document.getElementById('root')
 );
 
